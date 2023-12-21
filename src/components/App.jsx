81d556f5d59416586105596146d14e7690a7b434
VladislavGuliy <vladgulij777@gmail.com>
2023-12-21 13:39:42 +0200

    Створення компоненту <TransactionHistory>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from './Data/user.json';
 import data from './Data/data.json';
 import friends from './Data/friends.json';
+import transactions from './Data/transactions.json';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
