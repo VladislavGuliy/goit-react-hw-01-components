2fadfca32230056012ea45976880bce008a80a9d
VladislavGuliy <vladgulij777@gmail.com>
2023-12-21 09:24:33 +0200

    Створення компоненту  <Statistics>

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+import { getRandomHexColor } from './getRandomHexColor';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            style={{ background: getRandomHexColor() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
